4ea66f99504741f356cf81676cfde0192c91923c
driver565839 <vicente.kaufhold@gmail.com>
2016-11-20 01:30:24 -0500

    Update keyPad.py
    Code Overhaul

--- keyPad.py
+++ keyPad.py
@@ -14,29 +14,49 @@ def readcode():
     #Reads in numbers until the pound is pressed. Star resets
     attempt = ''
     i = 0
+    exit = 0
+    brk = 0
     start = time.time()  #Start the timer
     while True:  #Loop until pound sign or 60 seconds
         #return '1869'
         key = kp.getKey()
+        
         while (key) == None:
             key = kp.getKey()
+            end = time.time()  #End the timer
+            if(end-start > 60):  #Timeout if more than a minute elapses
+                brk = 1
+                break
+                
+        if(brk == 1)
+            break
+            
         #key = kp.getKey()
         #Get the key pressed
         print("Key pressed:", key,attempt)
         key = str(key)
         if key == '*':  #Reset
+            exit +=1
+            if(exit >= 2):
+                return 'exit'
             attempt = ''
+            start = time.time()
         elif key == '#':  #Return the final number
+            print("attempt:",attempt)
             return attempt
         else:
+            exit = 0
             attempt += key  #Add the key press to the code
 
-        end = time.time()  #End the timer
-        if(end-start > 60):  #Timeout if more than a minute elapses
-            break
+        
 
         while kp.getKey() != None: #Wait until the key is released
-            a=0
+            end = time.time()  #End the timer
+            if(end-start > 60):  #Timeout if more than a minute elapses
+                brk = 1
+                break
+        if(brk == 1)
+            break
     return 'Error: Timeout'
 
 # use 'GPIO naming'
@@ -72,40 +92,40 @@ attempt = '0000'
 
 while True:
     checkKeypad = kp.getKey()
-    while (checkKeypad) == None:
+    while (checkKeypad) != '*':
         checkKeypad = kp.getKey()
     #print(checkKeypad)
     if checkKeypad == '*':    #If its the start key
         print("Star Pressed")
         attempt = readcode()  #Run the function to read in the keys
     good = 0
-    print("attempt: ", attempt)
+    #print("attempt: ", attempt)
     for i in range(len(lst)):  #Loop through to see if the passcode matches anyones
         if(attempt == lst[i][1]):
             good = 1
             break
     #print("Good?: ",good)
+    if(attempt == 'exit')
+        continue
+        
     if good == 1:  #If the code is good
         #Do the second check.
         #Text code in
         newcode =  str(int(9999*random.random()))     
         print("newcode",newcode)
-        print("HERE")
         message = client.messages.create(to=lst[i][0], from_="+14128524518",body=newcode)
         print("HERE2")
-        checkKeypad = kp.getKey()  #Read in the keyvalue
-        #checkKeypad = '*'
-        if checkKeypad == "*":  #If its the start key
-            #print("SECOND STAR")
-            attempt = readcode()  #Run the function to read in the keys
+            attempt = readcode()
+            #checkKeypad = '*'
         if attempt == newcode:  #If the code is good
             wiringpi.pwmWrite(18, 150)  #Open the door
-            #print("OPEN DOOR")
+            print("OPEN DOOR")
             time.sleep(5)
-            #print("CLOSE DOOR")
+            print("CLOSE DOOR")
             wiringpi.pwmWrite(18, 90)  #Close the door
-
-
+    
+    else:
+        readcode()
     
 
 RPi.GPIO.cleanup()  #THis needs to be run last
