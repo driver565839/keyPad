61eadd5503f54e7c6b17747e3ca36b59edcb05cb
driver565839 <vicente.kaufhold@gmail.com>
2016-11-20 09:54:15 -0500

    Update
    Removed superfluous code

--- keyPad.py
+++ keyPad.py
@@ -116,7 +116,6 @@ wiringpi.pwmSetRange(2000)
 
 RPi.GPIO.setmode (RPi.GPIO.BCM)
 
-RPi.GPIO.setup(2, RPi.GPIO.IN, pull_up_down=RPi.GPIO.PUD_UP)
 RPi.GPIO.setup(17, RPi.GPIO.OUT)
 RPi.GPIO.setup(20, RPi.GPIO.OUT)
 wiringpi.pwmWrite(18, 90)  #Close the door
